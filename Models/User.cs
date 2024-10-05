424db7c056434243e2c51b4618e134573ce7537b
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-10-04 22:15:37 -0300

    feat: add User model

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pruebaidwm.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Rut { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
